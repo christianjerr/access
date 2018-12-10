2f33c36a46a46a44ea1b1f3437e5e55a02f4ec5f
Christian <christianjerr@yahoo.com>
2018-12-11 01:11:13 +0800

    signing out

--- js/script.js
+++ js/script.js
@@ -70,7 +70,7 @@ campusButton.addEventListener('click' , function(){
 			document.getElementsByTagName('header')[0].setAttribute('style' , 'margin-bottom : 0px ; height : 100vh');	
 		
 		}else{
-			document.getElementsByTagName('header')[0].setAttribute('style' , 'margin-bottom : 1100px ; height : 100vh');	
+			document.getElementsByTagName('header')[0].setAttribute('style' , 'margin-bottom : 1180px ; height : 100vh');	
 		}
 		Array.from(hiddenCampus).forEach(function(item){
 			item.setAttribute('style' , 'opacity : 0 ; visibility : hidden ; transition : .5s ;')
